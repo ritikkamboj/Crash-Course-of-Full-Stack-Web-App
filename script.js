b3b099c05db102c6c596ac2100d51744f9dca6f9
Aashish <ritikkamboj4314@gmail.com>
2023-03-03 18:21:21 +0530

    learned forEach and map , but the use of arrow function inside the map is awesome 😃😃👌

--- script.js
+++ script.js
@@ -159,3 +159,93 @@ const factObj2 = {
 };
 
 console.log(factObj2.summary());
+
+// ForEach function
+//forEach is a function and we also define function inside it , so basically its function inside function , wow 😲
+
+let arr = [2,4,6,8];
+
+ const arr1=arr.forEach(function(el)
+{
+  console.log(el);
+  // retrun (el+1);  // it showing runtime error 
+})
+// but we prefer here map() method ..
+
+arr2= arr.map(function(el){
+
+  return (el*10);
+});
+
+console.log(arr2);
+
+// let's use arrow fucntion in above function 
+
+//  const arr3 = (el) => el*5;
+
+//  console.log(arr3(...arr));    wrong implementation 
+
+let arr3 = arr. map((el)=> el*5);
+
+console.log(arr3);
+
+
+// below is the array of objects, wow😲👌  .
+const CATEGORIES = [
+  { name: "technology", color: "#3b82f6" },
+  { name: "science", color: "#16a34a" },
+  { name: "finance", color: "#ef4444" },
+  { name: "society", color: "#eab308" },
+  { name: "entertainment", color: "#db2777" },
+  { name: "health", color: "#14b8a6" },
+  { name: "history", color: "#f97316" },
+  { name: "news", color: "#8b5cf6" },
+];
+
+// we want an array which have only name  , we can do that with help of map method 
+
+
+const arr4 = CATEGORIES.map((el)=>el.name);
+console.log(arr4);
+
+// we have to calculate the age of fact from data.js file 
+
+const initialFacts = [
+  {
+    id: 1,
+    text: "React is being developed by Meta (formerly facebook)",
+    source: "https://opensource.fb.com/",
+    category: "technology",
+    votesInteresting: 24,
+    votesMindblowing: 9,
+    votesFalse: 4,
+    createdIn: 2021,
+  },
+  {
+    id: 2,
+    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
+    source:
+      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
+    category: "society",
+    votesInteresting: 11,
+    votesMindblowing: 2,
+    votesFalse: 0,
+    createdIn: 2019,
+  },
+  {
+    id: 3,
+    text: "Lisbon is the capital of Portugal",
+    source: "https://en.wikipedia.org/wiki/Lisbon",
+    category: "society",
+    votesInteresting: 8,
+    votesMindblowing: 3,
+    votesFalse: 1,
+    createdIn: 2015,
+  },
+];
+
+const arr6 = initialFacts.map((el)=>calcAge(el.createdIn));
+
+console.log(arr6);
+
+
