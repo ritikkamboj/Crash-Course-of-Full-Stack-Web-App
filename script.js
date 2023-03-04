3a413ec6fdf61071b546fc5f21a0a2ddda94dd08
Aashish <ritikkamboj4314@gmail.com>
2023-03-04 23:50:55 +0530

    creating DOM elements(used map() function from initialFacts array of objects  , wow , impressive , amazing 😲❤❤

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
 
+const initialFacts = [
+  {
+    id: 1,
+    text: "React is being developed by Meta (formerly facebook)",
+    source: "https://opensource.fb.com/",
+    category: "technology",
+    votesInteresting: 24,
+    votesMindblowing: 9,
+    votesFalse: 4,
+    createdIn: 2021,
+  },
+  {
+    id: 2,
+    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
+    source:
+      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
+    category: "society",
+    votesInteresting: 11,
+    votesMindblowing: 2,
+    votesFalse: 0,
+    createdIn: 2019,
+  },
+  {
+    id: 3,
+    text: "Lisbon is the capital of Portugal",
+    source: "https://en.wikipedia.org/wiki/Lisbon",
+    category: "society",
+    votesInteresting: 8,
+    votesMindblowing: 3,
+    votesFalse: 1,
+    createdIn: 2015,
+  },
+];
 
 console.log("thankful , gratefull , blessfull ");
 
+//Selecting DOM elements 
 const btn = document.querySelector(".btn-open");
 
 console.log(btn);
@@ -9,11 +43,19 @@ console.dir(btn);
 
 const factsList = document.querySelector(".facts-list");
 console.dir(factsList);
+
+//Creating(wow 😲) DOM elements . 
 factsList.innerHTML="";
+// factsList.insertAdjacentHTML("afterbegin","<li> Aashish Kumar </li>");
+// factsList.insertAdjacentHTML("afterbegin","<li> Ritik kamboj </li>");
+
+const htmlArr= initialFacts.map((fact)=> `<li class="fact">${fact.text}</li>`);
+console.log(htmlArr);
 
 
 const form = document.querySelector(".fact-form");
 
+// Toggle Form visibility 
 btn.addEventListener("click", function () {
 
   if (form.classList.contains("hidden")) {
