c9dd6863318e7796b32c299905d2c6c04b9dc9cf
ritik4314 <ritikkamboj4314@gmail.com>
2023-03-28 00:36:58 +0530

    made first react component and made appeared on UI too

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./style.css";
+
+
+
+function App() {
+  return (
+    <header className="header">
+      <div className="logo">
+        <img src="/logo.png" alt="logo" />
+        <h1>Today I Learned</h1>
+      </div>
+      <button className="btn btn-large btn-open">Share a Fact</button>
+    </header>
+  );
+}
+
+export default App;
