629a3876ea0c3ec80698fc25683cb842e809cdbc
ritik4314 <ritikkamboj4314@gmail.com>
2023-05-13 18:51:27 +0530

    separate the li as a separate component, replace .container to #root in css file

--- src/App.js
+++ src/App.js
@@ -85,38 +85,47 @@ function FactList() {
       <section>
         <ul className="facts-list">
           {facts.map((fact) => (
-            <li key={fact.id} className="fact">
-              <p>
-                {fact.text}
-                <a
-                  className="source"
-                  href={fact.source}
-                  target="_blank"
-                >
-                  (Source)
-                </a>
-              </p>
-              <span
-                className="tag"
-                style={{
-                  backgroundColor: CATEGORIES.find(
-                    (cat) => cat.name === fact.category
-                  ).color,
-                }}
-              >
-                {fact.category}
-              </span>
-
-              <div className="vote-buttons">
-                <button>👍{fact.votesInteresting}</button>
-                <button>🤯{fact.votesMindblowing}</button>
-                <button>⛔️{fact.votesFalse}</button>
-              </div>
-            </li>
+            <Fact key={fact.id} fact={fact} />
+            
           ))}
         </ul>
+        <p>There are {facts.length} facts in initialFacts array of objects .</p>
       </section>
       <CategoryFilter />
     </>
   );
+}
+
+
+function Fact({fact})
+{
+  
+  return <li  className="fact">
+  <p>
+    {fact.text}
+    <a
+      className="source"
+      href={fact.source}
+      target="_blank"
+    >
+      (Source)
+    </a>
+  </p>
+  <span
+    className="tag"
+    style={{
+      backgroundColor: CATEGORIES.find(
+        (cat) => cat.name === fact.category
+      ).color,
+    }}
+  >
+    {fact.category}
+  </span>
+
+  <div className="vote-buttons">
+    <button>👍{fact.votesInteresting}</button>
+    <button>🤯{fact.votesMindblowing}</button>
+    <button>⛔️{fact.votesFalse}</button>
+  </div>
+</li>
 }
